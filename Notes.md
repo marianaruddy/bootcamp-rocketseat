14cdd67fa1ba87c9a341cf8d99cd47c11cfd716b
mariana.ruddy <mariana.ruddy@b2wdigital.com>
2020-11-06 10:35:44 -0300

    Métodos HTTP

---
+++ Notes.md
@@ -0,0 +1,7 @@
+## Métodos HTTP
+- GET
+- POST
+- PUT/PATCH:
+    PUT é para atualizar um reucrso por completo, PATCH é para atualizar uma informação especifica
+- DELETE
+
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,35 @@ const express = require("express");
 const app = express();
 
 app.get('/projects', (request, response) => {
-    return response.json({ message: 'Hello world' });
+    return response.json([
+        'p1',
+        'p2',
+    ]);
 });
 
-app.listen(3333);
+app.post('/projects', (request, response) => {
+    return response.json([
+        'p1',
+        'p2',
+        'p3',
+    ]);
+});
+
+app.put('/projects/:id', (request, response) => {
+    return response.json([
+        'p1',
+        'p2',
+        'p3',
+    ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json([
+        'p1',
+        'p2',
+    ]);
+});
+
+app.listen(3333, () => {
+    console.log('Back-end started!');
+});
