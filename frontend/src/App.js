de5eca162ee110f80b54fc080c2d517b99f3bff0
mariana.ruddy <mariana.ruddy@b2wdigital.com>
2020-12-29 10:47:15 -0300

    Importando CSS e imagens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,8 @@
 import React, { useState } from 'react';
+
+import './App.css';
+import backgroundImage from './assets/background.jpeg';
+
 import Header from './components/Header';
 
 function App(){
@@ -15,6 +19,9 @@ function App(){
     return (
     <>
         <Header title="Projects" />
+
+        <img width={300} src={backgroundImage} />
+    
         <ul>
             {projects.map(project => (
                 <li key={project}>
